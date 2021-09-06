c3b3cfb399937460e68c99ba73977e172cc09543
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-06 23:59:58 +0300

    Update ERC1155.sol

--- ERC1155.sol
+++ ERC1155.sol
@@ -16,3 +16,27 @@ contract ERC1155 is IERC1155, ERC165, CommonConstants
 
     // owner => (operator => approved)
     mapping (address => mapping(address => bool)) internal operatorApproval;
+
+bytes4 constant private INTERFACE_SIGNATURE_ERC165 = 0x01ffc9a7;
+
+    /*
+        bytes4(keccak256("safeTransferFrom(address,address,uint256,uint256,bytes)")) ^
+        bytes4(keccak256("safeBatchTransferFrom(address,address,uint256[],uint256[],bytes)")) ^
+        bytes4(keccak256("balanceOf(address,uint256)")) ^
+        bytes4(keccak256("balanceOfBatch(address[],uint256[])")) ^
+        bytes4(keccak256("setApprovalForAll(address,bool)")) ^
+        bytes4(keccak256("isApprovedForAll(address,address)"));
+    */
+    bytes4 constant private INTERFACE_SIGNATURE_ERC1155 = 0xd9b67a26;
+
+    function supportsInterface(bytes4 _interfaceId)
+    public
+    view
+    returns (bool) {
+         if (_interfaceId == INTERFACE_SIGNATURE_ERC165 ||
+             _interfaceId == INTERFACE_SIGNATURE_ERC1155) {
+            return true;
+         }
+
+         return false;
+    }
