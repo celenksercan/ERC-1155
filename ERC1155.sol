4a2bb7862e3096cd6ae0864b99b88a28de848983
celenksercan <52419173+celenksercan@users.noreply.github.com>
2021-09-06 23:59:41 +0300

    Update ERC1155.sol

--- ERC1155.sol
+++ ERC1155.sol
@@ -5,3 +5,14 @@ import "./Address.sol";
 import "./Common.sol";
 import "./IERC1155TokenReceiver.sol";
 import "./IERC1155.sol";
+
+contract ERC1155 is IERC1155, ERC165, CommonConstants
+{
+    using SafeMath for uint256;
+    using Address for address;
+
+    // id => (owner => balance)
+    mapping (uint256 => mapping(address => uint256)) internal balances;
+
+    // owner => (operator => approved)
+    mapping (address => mapping(address => bool)) internal operatorApproval;
